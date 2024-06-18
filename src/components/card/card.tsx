4aa259f7f189e70ea318b41301110264910ed39e
Leonardo Betetto <60801645+lmbbetetto@users.noreply.github.com>
2024-06-18 20:23:33 -0300

    commit uptade url

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -3,11 +3,11 @@ import style from './style.module.css'
 export function Card() {
     return (
         <section className={style.card}>
-            <img src="src\assets\fema.png" alt="LogoFema" className={style.img1} />
+            <img src="../../src/assets/fema.png" alt="LogoFema" className={style.img1} />
             <p>Trabalho Angular</p>
             <p>3º ADS - Junho - 2024</p>
             <p>Leonardo Manoel Batista Betetto - 2211550156</p>
-            <img src="src\assets\angular.png" alt="logoAngular" className={style.img2} />
+            <img src="../../src/assets/angular.png" alt="logoAngular" className={style.img2} />
         </section>
     )
 }
